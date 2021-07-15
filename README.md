a674051e098ff582e428402ebb97c6248127af0f
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-07-15 08:57:53 +0600

    refactor code & cleanify

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # shopping-cart with DOM
 
--I played here with DOM (Document Object Model). I caught many HTML elements by calling those ids -with help of DOM.
--I build functionality with DOM.
--eventually, it's prepared for use by the user.
+- I played here with DOM (Document Object Model). I caught many HTML elements by calling those ids - with help of DOM.
+- I build functionality with DOM.
+- eventually, it's prepared for use by the user.
--- js/script.js
+++ js/script.js
@@ -1,96 +1,51 @@
-// phone & case quantity increment & decrement button
-const phoneQtyIncrementBtn = document.getElementById('phone-qty-increment-btn');
-const phoneQtyDecrementBtn = document.getElementById('phone-qty-decrement-btn');
-const caseQtyIncrementBtn = document.getElementById('case-qty-increment-btn');
-const caseQtyDecrementBtn = document.getElementById('case-qty-decrement-btn');
-
-
-// phone quantity & Price determine
-phoneQuantityAndPrice(phoneQtyIncrementBtn);
-phoneQuantityAndPrice(phoneQtyDecrementBtn);
-
-function phoneQuantityAndPrice(quantityBtn) {
-	quantityBtn.addEventListener('click', function () {
-		let quantity = document.getElementById('phone-qty').value;
-		let quantityNumber = parseInt(quantity);
-
-		if (quantityBtn.id == 'phone-qty-increment-btn') {
-			quantityNumber++;
-		} else {
-			if (quantityNumber === 1) {
-				return;
-			} else {
-				quantityNumber--;
-			}
+const productChangeHandler = (isIncrement, product) => {
+	const productQuantity = getInputValue(product);
+	let productNewQuantity = productQuantity;
+	if (isIncrement) {
+		productNewQuantity++;
+	} else {
+		if (productNewQuantity > 0) {
+			productNewQuantity--;
 		}
-		document.getElementById('phone-qty').value = quantityNumber;
+	}
 
-		let phonePrice = 1219 * quantityNumber; // every phone price = 1219 taka
-		document.getElementById('phone-price').innerText = phonePrice;
+	document.getElementById(`${product}-quantity`).value = productNewQuantity;
 
-		// phone subtotal price
-		phoneSubtotal(phonePrice);
-	});
-}
+	let totalProductPrice;
+	if (product == 'phone') {
+		totalProductPrice = productNewQuantity * 1219;
+	}
+	if (product == 'case') {
+		totalProductPrice = productNewQuantity * 59;
+	}
 
-// case quantity & Price determine
-caseQuantityAndPrice(caseQtyIncrementBtn);
-caseQuantityAndPrice(caseQtyDecrementBtn);
+	document.getElementById(`${product}-price`).innerText = totalProductPrice;
+	productCalculation();
+};
 
-function caseQuantityAndPrice(quantityBtn) {
-	quantityBtn.addEventListener('click', function () {
-		let quantity = document.getElementById('case-qty').value;
-		let quantityNumber = parseInt(quantity);
+const productCalculation = () => {
+	const phoneQuantity = getInputValue('phone');
+	const caseQuantity = getInputValue('case');
 
-		if (quantityBtn.id == 'case-qty-increment-btn') {
-			quantityNumber++;
-		} else {
-			if (quantityNumber === 1) {
-				return;
-			} else {
-				quantityNumber--;
-			}
-		}
-		document.getElementById('case-qty').value = quantityNumber;
-
-		let casePrice = 59 * quantityNumber; // every case price 59 taka
-		document.getElementById('case-price').innerText = casePrice;
-
-		//case subtotal price
-		caseSubtotal(casePrice);
-	});
-}
-
-function phoneSubtotal(phonePrice) {
-	let casePrice = document.getElementById('case-price').innerText;
-	let casePriceNumber = parseFloat(casePrice);
-	let subTotal = casePriceNumber + phonePrice;
-	taxAndTotal(subTotal);
-}
-
-function caseSubtotal(casePrice) {
-	let phonePrice = document.getElementById('phone-price').innerText;
-	let phonePriceNumber = parseFloat(phonePrice);
-	let subTotal = phonePriceNumber + casePrice;
-
-	// tax and total
-	taxAndTotal(subTotal);
-}
-
-function taxAndTotal(subTotal) {
+	const subTotal = phoneQuantity * 1219 + caseQuantity * 59;
 	document.getElementById('sub-total').innerText = subTotal;
-	// tax
-	let tax = parseFloat((subTotal / 95).toFixed(2));
+
+	const tax = Math.round(subTotal * 0.1);
 	document.getElementById('tax').innerText = tax;
-	// Total
-	let total = subTotal + tax;
-	document.getElementById('final-total').innerText = total;
-}
+
+	const grandTotal = subTotal + tax;
+	document.getElementById('grand-total').innerText = grandTotal;
+};
+
+const getInputValue = (product) => {
+	const productInputQuantity = document.getElementById(`${product}-quantity`);
+	return (productQuantity = parseInt(productInputQuantity.value));
+};
 
 // checkout alert
 const checkoutBtn = document.getElementById('checkout-btn');
 const message = document.getElementById('message');
-checkoutBtn.addEventListener('click', function(){
-    alert('Do you purchase these items?');
-    message.innerHTML = 'Thank you! your order ready to pack.'
-})
+checkoutBtn.addEventListener('click', function () {
+	alert('Do you purchase these items?');
+	message.innerHTML = 'Thank you! your order ready to pack.';
+});
